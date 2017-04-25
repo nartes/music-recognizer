7c9cba846b00b3b376e3223626293f329d23d118
Siarhei Siniak <serega.belarus@gmail.com>
2017-04-25 12:21:44 +0300

    Add end-to-end predict for sin

--- src/e2e-amt/neural_nets_script.py
+++ src/e2e-amt/neural_nets_script.py
@@ -18,27 +18,15 @@ def gen_sin_train_and_test(NOISE_AMPL = 0.5 * 1e-2,\
 
     return X, Y
 
-def test1():
-    nb_classes = 3
-    batch_size = 128
-    nb_epoch = 2
-    X_train, Y_train = gen_sin_train_and_test(L = 60000, NOISE_AMPL = 5e-2)
-    X_test, Y_test = gen_sin_train_and_test(L = 100)
-    print(X_train.shape[0], 'train samples')
-    print(X_test.shape[0], 'test samples')
-
-    Y_train = keras.utils.np_utils.to_categorical(\
-            numpy.array(numpy.round(Y_train), dtype = numpy.int), nb_classes)
-    Y_test = keras.utils.np_utils.to_categorical(\
-            numpy.array(numpy.round(Y_test), dtype = numpy.int), nb_classes)
-
+def sin_model():
     model = keras.models.Sequential()
     for k in numpy.arange(1):
-        DW = 5
+        DW = 20
         if k > 1:
-            model.add(keras.layers.Dense(DW, init = 'uniform'))
+            model.add(keras.layers.Dense(DW, kernel_initializer = 'uniform'))
         else:
-            model.add(keras.layers.Dense(DW, input_dim = 2, init = 'uniform'))
+            model.add(keras.layers.Dense(DW, input_dim = 2,\
+                      kernel_initializer = "uniform"))
         model.add(keras.layers.Activation('relu'))
         model.add(keras.layers.Dropout(0.3))
     model.add(keras.layers.Dense(3))
@@ -49,9 +37,74 @@ def test1():
                   metrics = ['accuracy'])
 
     model.summary()
+
+    return model
+
+def sin_model2():
+    model = keras.models.Sequential()
+    for k in numpy.arange(1):
+        DW = 32
+        if k > 1:
+            model.add(keras.layers.Dense(DW, kernel_initializer = 'uniform'))
+        else:
+            model.add(keras.layers.Dense(DW, input_dim = 2,\
+                      kernel_initializer = "uniform"))
+        model.add(keras.layers.Activation('relu'))
+        model.add(keras.layers.Dropout(0.3))
+    model.add(keras.layers.Dense(10))
+    model.add(keras.layers.Activation('softmax'))
+    model.add(keras.layers.Dense(1))
+
+    model.compile(loss = 'mse',\
+                  optimizer = 'rmsprop',\
+                  metrics = ['mse'])
+
+    model.summary()
+
+    return model
+
+def test1():
+    nb_classes = 3
+    batch_size = 64
+    nb_epoch = 1
+    X_train, Y_train = gen_sin_train_and_test(L = 60000, NOISE_AMPL = 5e-2)
+    X_test, Y_test = gen_sin_train_and_test(L = 100)
+    print(X_train.shape[0], 'train samples')
+    print(X_test.shape[0], 'test samples')
+
+    Y_train = keras.utils.np_utils.to_categorical(\
+            numpy.array(numpy.round(Y_train), dtype = numpy.int), nb_classes)
+    Y_test = keras.utils.np_utils.to_categorical(\
+            numpy.array(numpy.round(Y_test), dtype = numpy.int), nb_classes)
+
+    model = sin_model()
     model.fit(X_train, Y_train,\
-              nb_epoch = nb_epoch,
-              batch_size = batch_size,
+              epochs = nb_epoch,\
+              batch_size = batch_size,\
+              verbose = 1, validation_data = (X_test, Y_test))
+    score = model.evaluate(X_test, Y_test, verbose = 0)
+    print('Test score:', score[0])
+    print('Test accuracy:', score[1])
+
+    return model
+
+def test2():
+    batch_size = 64
+    nb_epoch = 1
+    X_train, Y_train = gen_sin_train_and_test(L = 60000, NOISE_AMPL = 5e-2)
+    X_test, Y_test = gen_sin_train_and_test(L = 100)
+    print(X_train.shape[0], 'train samples')
+    print(X_test.shape[0], 'test samples')
+
+    #Y_train = keras.utils.np_utils.to_categorical(\
+    #        numpy.array(numpy.round(Y_train), dtype = numpy.int), nb_classes)
+    #Y_test = keras.utils.np_utils.to_categorical(\
+    #        numpy.array(numpy.round(Y_test), dtype = numpy.int), nb_classes)
+
+    model = sin_model2()
+    model.fit(X_train, Y_train,\
+              epochs = nb_epoch,\
+              batch_size = batch_size,\
               verbose = 1, validation_data = (X_test, Y_test))
     score = model.evaluate(X_test, Y_test, verbose = 0)
     print('Test score:', score[0])
