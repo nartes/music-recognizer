6d2a27cc07a96c073a8cded3dfab0a716c5182da
Siarhei Siniak <serega.belarus@gmail.com>
2017-05-14 00:12:41 +0300

    Add dumb dnn test (#8)
    * Add init file for ipython
    
    * Add cqt example
    
    * Add nmf application with cqt
    
    * Add wav_to_cqt
    
    * Add bins params to cqt
    
    * Add dumb amt model and util function
    
      1. Add dumb amt model that generates 88 features
        to present an acoustic model. And then process the output
        through additional RNN, that presents the music language model.
    
      2. Add util function to normalize CQT across all dimensions.
    
    * Add y_seq generation from midi data
    
      1. Add scanning of MAPS dir
    
      2. Add parsing of a txt midi notation
    
      3. Add generation of y_seq matrix from notes matrix
    
      4. Add plotting of y_seq
    
      5. Fix keras API errors in model generation
    
    * Add model training and validation
    
      1. Add model fitting in keras
    
      2. Model doesn't converge in a right direction. Need to fix errors.
    
    * Add multi file training
    
      1. Add an iteration over globbed files
    
      2. Fix an incorrect normalization of the CQT
    
    * Add dataset preprocessing and caching
    
      1. Add caching for the CQT and ground truth
        transcription generation
    
    * Visualize the validation prediction
    
      1. Add plotting of transcriptions, both of predicted and ground truth
    
      2. Modify the network to make it simpler
    
      3. The network still has a wrong loss, since it converges
        quickly, but the result is different. Sometimes it is right,
        but not always.
    
    * Add reasonable metrics and loss
    
      1. Add frame accuracy based metrics
    
      2. Modify caching functions
    
      3. Add training data provisioning for a test

--- src/e2e-amt/neural_nets_script.py
+++ src/e2e-amt/neural_nets_script.py
@@ -4,6 +4,13 @@ import keras.layers
 import keras.optimizers
 import keras.utils
 import numpy
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import nmf_tools
+import glob
+import sys
+import io
 
 def gen_seq(NOISE_AMPL = 0.5 * 1e-2,\
             ORDER = 2, AHEAD = 1, N = 4,
@@ -129,3 +136,262 @@ def print_test_samples(N, model):
               '\n\n***\n\n',\
               'a[1] - b\n\n',
               a[1] - b)
+
+def dumb_amt_model():
+    model = keras.models.Sequential()
+
+    model.add(keras.layers.SimpleRNN(
+            batch_input_shape = (None, 1, 252),
+            units = 32,
+            activation = 'relu'))
+    model.add(keras.layers.Dense(512, activation = 'relu'))
+    model.add(keras.layers.Dense(88, activation = 'softmax'))
+    model.add(keras.layers.Reshape(target_shape = (88, 1)))
+    model.add(keras.layers.SimpleRNN(88, activation = 'relu'))
+
+    model.compile(loss = 'mae',\
+                  optimizer = 'adadelta',\
+                  metrics = ['mse', 'mae'])
+
+    return model
+
+def dnn_amt_model(loss = 'binary_crossentropy',
+                  optimizer = 'rmsprop',
+                  metrics = ['accuracy']):
+    model = keras.models.Sequential()
+
+    model.add(keras.layers.Dense(
+            batch_input_shape = (None, 1, 252),
+            units = 64,
+            kernel_initializer = 'uniform',
+            activation = 'relu'))
+    model.add(keras.layers.Dense(512, activation = 'relu'))
+    model.add(keras.layers.Dropout(0.3))
+    model.add(keras.layers.Dense(512, activation = 'relu'))
+    model.add(keras.layers.Dropout(0.3))
+    model.add(keras.layers.Dense(512, activation = 'relu'))
+    model.add(keras.layers.Dropout(0.3))
+    model.add(keras.layers.Dense(88, activation = 'sigmoid'))
+    model.add(keras.layers.Reshape(target_shape = (88,)))
+
+    model.compile(loss = loss, optimizer = optimizer, metrics = metrics)
+
+    return model
+
+def parse_notes(txt_fname,
+                num_frames,
+                n_bins = 252,
+                bins_per_octave = 36):
+    notes = numpy.loadtxt(fname = txt_fname, skiprows = 1, ndmin = 2)
+
+    y_seq = nmf_tools.maps_notes_to_y_seq(notes)
+
+    Y_test = y_seq[:, :num_frames].T
+
+    return Y_test, y_seq
+
+def process_C(wav_fname,
+              n_bins = 252,
+              bins_per_octave = 36):
+    C = nmf_tools.wav_to_cqt(fname = wav_fname,
+                             n_bins = n_bins,
+                             bins_per_octave = bins_per_octave)[1]
+    C = numpy.abs(C)
+    C = nmf_tools.normalize_cqt(C)
+
+    C += numpy.min(C)
+
+    X = C.T.reshape(-1, 1, C.shape[0])
+
+    return X
+
+def set_cached_data(txt_fname = None,
+                    wav_fname = None,
+                    X = None,
+                    Y_test = None,
+                    ignore = False,
+                    cache_file = None):
+    if cache_file is None:
+        cache_file = txt_fname[:-4] + '.meta.npz'
+
+    if len(glob.glob(cache_file)) >= 1 and not ignore:
+        return False
+
+    try:
+        numpy.savez(cache_file, X = X, Y_test = Y_test)
+        return True
+    except:
+        return False
+
+def get_cached_data(txt_fname = None, wav_fname = None, cache_file = None):
+    if cache_file is None:
+        cache_file = txt_fname[:-4] + '.meta.npz'
+
+    try:
+        metadata = numpy.load(cache_file)
+        X = metadata['X']
+        Y_test = metadata['Y_test']
+        return X, Y_test
+    except:
+        return None, None
+
+def generate_training_data(n_bins = 252,
+                  bins_per_octave = 36,
+                  hop_length = 512,
+                  sr = 16000,
+                  o_sr = 44100,
+                  fmin = librosa.note_to_hz('C1'),
+                  max_files = 10,
+                  batch_size = 1,
+                  glob_param = 'ISOL/NO',
+                  ignore = False):
+    txts = glob.glob(\
+            'tmp/MAPS-dataset/*/' + glob_param + '/*.txt')
+    wavs = glob.glob(\
+            'tmp/MAPS-dataset/*/' + glob_param + '/*.wav')
+
+    if max_files == -1 or max_files is None:
+        max_files = len(txts)
+
+    X = None
+    Y_test = None
+
+    for txt_fname in txts:
+        wav_fname = txt_fname[:-4] + '.wav'
+
+        print(f'Left {max_files}')
+        print(f'File: {txt_fname} started.')
+
+        cur_X = None
+        cur_Y_test = None
+
+        if not ignore:
+            cur_X, cur_Y_test = get_cached_data(txt_fname, wav_fname)
+
+        if cur_X is None or cur_Y_test is None:
+            cur_X = process_C(wav_fname = wav_fname,
+                          n_bins = n_bins,
+                          bins_per_octave = bins_per_octave)
+            cur_Y_test, y_seq = parse_notes(\
+                    txt_fname = txt_fname,
+                    n_bins = n_bins,
+                    bins_per_octave = bins_per_octave,
+                    num_frames = cur_X.shape[0])
+
+            set_cached_data(txt_fname = txt_fname,
+                            wav_fname = wav_fname,
+                            X = cur_X,
+                            Y_test = cur_Y_test,
+                            ignore = ignore)
+        else:
+            print('Found a cached file.')
+
+        if X is None:
+            X = cur_X
+        else:
+            X = numpy.vstack((X, cur_X))
+
+        if Y_test is None:
+            Y_test = cur_Y_test
+        else:
+            Y_test = numpy.vstack((Y_test, cur_Y_test))
+
+        print(f'File: {txt_fname} ended.')
+
+        max_files -= 1
+        if max_files == 0:
+            break
+
+    return X, Y_test
+
+def dumb_amt_test(model = dumb_amt_model(),
+                  n_bins = 252,
+                  bins_per_octave = 36,
+                  hop_length = 512,
+                  sr = 16000,
+                  o_sr = 44100,
+                  fmin = librosa.note_to_hz('C1'),
+                  plot_each = False,
+                  epochs = 1,
+                  max_files = 10,
+                  val_max_files = 3,
+                  batch_size = 1,
+                  glob_param = 'ISOL/NO',
+                  ignore = False,
+                  training_data = None):
+
+    X = None
+    Y_test = None
+
+    if training_data is None:
+        X, Y_test = generate_training_data(n_bins = n_bins,
+                bins_per_octave = bins_per_octave,
+                hop_length = hop_length,
+                sr = sr,
+                o_sr = o_sr,
+                fmin = fmin,
+                max_files = max_files,
+                batch_size = batch_size,
+                glob_param = glob_param,
+                ignore = ignore)
+    else:
+        X, Y_test = training_data
+
+    val_X, val_Y_test = generate_training_data(max_files = val_max_files,
+            ignore = ignore)
+
+    model.fit(X, Y_test, epochs = epochs, batch_size = batch_size, verbose = 1,
+              validation_data = (val_X, val_Y_test))
+
+    pred_Y = model.predict(val_X)
+
+    plt.subplot(131)
+    plt.imshow(val_X.squeeze())
+    plt.subplot(132)
+    plt.imshow(val_Y_test.squeeze())
+    plt.subplot(133)
+    plt.imshow(pred_Y.squeeze())
+    plt.show()
+
+    return model
+
+def plot_processing(p_y_seq,
+        y_seq,
+        sr = 16000,
+        bins_per_octave = 36,
+        hop_length = 512):
+    plt.figure()
+    plot_y_seq(y_seq, sr = sr, bins_per_octave = bins_per_octave,
+               hop_length = hop_length,
+               title = 'Ground truth midi transcription')
+
+
+    plt.figure()
+    plot_y_seq(p_y_seq, sr = sr, bins_per_octave = bins_per_octave,
+               hop_length = hop_length,
+               title = 'Predicted midi transcription')
+
+    plt.show()
+
+def plot_y_seq(y_seq, sr, bins_per_octave, hop_length, title):
+    librosa.display.specshow(librosa.amplitude_to_db(y_seq, ref = numpy.max),
+                             sr = sr, x_axis = 'time', y_axis = 'cqt_note',\
+                             bins_per_octave = bins_per_octave,
+                             hop_length = hop_length)
+    plt.title(title)
+    plt.tight_layout()
+
+def amt_frame_metrics(y_true, y_pred):
+    TP = keras.backend.sum(
+            keras.backend.minimum(y_true, y_pred),
+            axis = None)
+    TP_FN = keras.backend.sum(keras.backend.abs(y_true), axis = None)
+    TP_FP = keras.backend.sum(keras.backend.abs(y_pred), axis = None)
+
+    R = keras.backend.sum(TP / (TP_FN + 1e-6))
+
+    P = keras.backend.sum(TP / (TP_FP + 1e-6))
+
+    F = keras.backend.clip(2 * P * R / (P + R + 1e-6), 0.0, 1.0)
+
+    return {'P': P, 'R': R, 'F': F}
--- src/e2e-amt/nmf_tools.py
+++ src/e2e-amt/nmf_tools.py
@@ -121,3 +121,86 @@ def plot_w_matrix(FS, N, MFS, R, W):
         plt.plot(fhz, W[:, k - 1].T)
         plt.xlabel('Hz')
         plt.ylabel('amplitude')
+
+def test3(fname = 'tmp/organ.wav', sr = 44100):
+    y = librosa.load(fname, sr, mono = True)[0]
+    C = librosa.cqt(y, sr = sr)
+    librosa.display.specshow(librosa.amplitude_to_db(C, ref = numpy.max),
+                             sr = sr, x_axis = 'time', y_axis = 'cqt_note')
+    plt.colorbar(format = '%+2.0f dB')
+    plt.title('Constant-Q power spectrum')
+    plt.tight_layout()
+
+    plt.show()
+
+def test4(fname = 'tmp/organ.wav', sr = 44100, d_sr = 16000,\
+          R = 11, tol = 1e-2,\
+          max_iter = 100, attempts = 5,\
+          hop_length = 512, bins_per_octave=12, n_bins = 84):
+    C = wav_to_cqt(fname, sr, downsample_sr = d_sr,\
+                   n_bins = n_bins, bins_per_octave = bins_per_octave)[1]
+
+    W, H = nmf(numpy.abs(C), R, tol, max_iter, attempts)[:2]
+
+    L = C.shape[1]
+    HS = hop_length
+    MFS = C.shape[0]
+    FS = sr
+    TL = 0.0
+    M = 2048
+    N = 2048
+
+    librosa.display.specshow(librosa.amplitude_to_db(C, ref = numpy.max),
+                             sr = sr, x_axis = 'time', y_axis = 'cqt_note',\
+                             bins_per_octave = bins_per_octave,
+                             hop_length = hop_length)
+    plt.colorbar(format = '%+2.0f dB')
+    plt.title('Constant-Q power spectrum')
+    plt.tight_layout()
+
+    plot_h_matrix(L, M, HS, FS, TL, R, H)
+    plot_w_matrix(FS, N, MFS, R, W)
+
+    plt.show()
+
+def wav_to_cqt(fname = 'tmp/organ.wav', original_sr = 44100,\
+        downsample_sr = 16000,\
+        n_bins = 84,\
+        bins_per_octave = 12,\
+        fmin = librosa.note_to_hz('C1')):
+    y = librosa.load(fname, original_sr, mono = True)[0]
+    y_ds = librosa.resample(y, original_sr, downsample_sr)
+    C = librosa.cqt(y_ds, sr = downsample_sr, n_bins = n_bins,\
+                    bins_per_octave = bins_per_octave)
+
+    return y, C, y_ds
+
+def normalize_cqt(C):
+    C = C - numpy.mean(C)
+    C = C / numpy.sqrt(numpy.var(C))
+    return C
+
+def maps_notes_to_y_seq(notes):
+    MINN = librosa.note_to_midi('C1')
+    MINT = 0.0
+    MAXT = 10.0
+    N_BINS = 252
+    FPS = 16000 / 512
+    y_seq = numpy.zeros((88, int(numpy.ceil(MAXT * FPS))), dtype = numpy.float)
+
+    for k in numpy.arange(notes.shape[0]):
+        cur_note = numpy.int(numpy.floor(numpy.float(notes[k][2]) - MINN))
+
+        if cur_note < 0:
+            continue
+
+        start_frame = numpy.int(numpy.floor(\
+                FPS * numpy.float(notes[k][0])\
+                ))
+        end_frame = numpy.int(numpy.floor(\
+                FPS * numpy.float(notes[k][1])\
+                ))
+        for t in numpy.arange(start_frame, end_frame + 1):
+            y_seq[cur_note][t] = 1.0;
+
+    return y_seq
